8262d835f540e0f86bf484705e0bc467326ddb0b
mattyD797 <ralph797@live.com>
2020-11-30 13:26:13 -0500

    Added JavaDoc to productlisting

--- src/ProductListing.java
+++ src/ProductListing.java
@@ -1,7 +1,23 @@
-
+/**
+ * Includes all the necessary methods for being a product in our listing. 
+ * @author Matt Duggan
+ *
+ */
 public interface ProductListing {
+	/**
+	 * Getter for the name of the product. 
+	 * @return The title 
+	 */
 	public String getTitle();
+	/**
+	 * Getter for the price of the product.
+	 * @return The price. 
+	 */
 	public double getPrice();
+	/**
+	 * Getter for the description of the product.
+	 * @return The description.
+	 */
 	public String getDescription();
 }
 
